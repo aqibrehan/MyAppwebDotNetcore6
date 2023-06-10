d9c49b1950c690bbf6e444e6d173e8999cc4c1b6
MuhammadAqibrehan <aqib.koko@gmail.com>
2023-06-10 23:43:04 +0500

    Initial commit

---
+++ MyAppWeb/Controllers/CategoryController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MyAppWeb.Data;
+using MyAppWeb.Models;
+
+namespace MyAppWeb.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private ApplicationDbContext _context;
+
+        public CategoryController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+        [HttpGet]
+        public IActionResult Index()
+        {
+            IEnumerable<Category> categories = _context.Categories;
+
+            return View(categories);
+        }
+        
+        public IActionResult Create()
+        {
+
+            return View();
+        }
+    }
+}
